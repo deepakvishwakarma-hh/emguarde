ff3aa0bdaaa90021c32c3ab95bf55bedf9fca2dd
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-09-22 13:19:11 +0530

    fix

--- components/FixedBg.jsx
+++ components/FixedBg.jsx
@@ -1,10 +1,10 @@
 const Fixedbg = () => {
   return (
     <section
-      className="md:h-[50vh] h-screen bg-cover bg-center bg-fixed flex items-end justify-end p-10"
+      className="md:h-[50vh] h-[50vh] bg-cover bg-center bg-fixed flex items-end justify-end p-10"
       style={{ backgroundImage: "url('/pexels-vecislavas-popa-1571459.jpg')" }}
     >
-      <div className="bg-white p-10 rounded-md max-w-3xl bg-opacity-90">
+      <div className="bg-white md:p-10 p-5 rounded-md max-w-3xl bg-opacity-90">
         <h5 className=" font-heading mb-2 text-xl font-semibold md:text-2xl">
           Eliminate Radiation from your Surroundings
         </h5>
