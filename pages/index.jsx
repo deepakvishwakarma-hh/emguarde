cbec7a281d8ed36e135b1ee34ac53226d0a94084
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2023-10-02 13:05:45 +0530

    specification

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,15 @@
 import SEO from "../components/SEO";
 import Buy from "../components/Buy";
 import Hero from "../components/Hero";
+import Video from "../components/Video";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import WhyBuilt from "../components/WhyBuild";
 import JoinOurEvent from "../components/JoinOurEvent";
 import Testimonials from "../components/Testimonials";
+import Specification from "../components/Specification";
 import HealthBenefits from "../components/HealthBenefits";
 import ImpactsOfRadiation from "../components/ImapactsOfRadiation";
-import Video from "../components/Video";
 export default function Home() {
   return (
     <>
@@ -19,6 +20,7 @@ export default function Home() {
         <WhyBuilt />
         <ImpactsOfRadiation />
         <HealthBenefits />
+        <Specification />
         <Video />
         <Testimonials />
         <JoinOurEvent />
